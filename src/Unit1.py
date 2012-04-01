b260e8eeefee5b1882b343c745888ba0118005c1
Michal Koperski <m.koperski@gmail.com>
2012-04-01 17:31:45 +0200

    Sense function w/o normialization

--- src/Unit1.py
+++ src/Unit1.py
@@ -1,10 +1,17 @@
-#Modify your code to create probability vectors, p, of arbitrary 
-#size, n. Use n=5 to verify that your new solution matches 
-#the previous one.
-
-p=[]
-n=5
-
-for i in range(n) :
-    p.append(1.0/n);
-print p
+#initial probabilities
+p = [0.2, 0.2, 0.2, 0.2, 0.2]
+#World`s map
+world = ['green', 'red', 'red', 'green', 'green']
+#Measurement
+Z = 'red'
+#Probability of sensnig state and being in that state
+pHit = 0.6
+#Probablity of sensing state and being in other state
+pMiss = 0.2
+def sense(p, Z):#Define a function sense return q
+    q = []
+    for i in range(len(world)):
+        hitted = Z == world[i]
+        q.append(p[i] * ((hitted * pHit) + (1-hitted)*pMiss))
+    return q
+print sense(p, Z)
