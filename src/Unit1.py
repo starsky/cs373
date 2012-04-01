09e75053749a368223a426a56b664aa42a03126e
Michal Koperski <m.koperski@gmail.com>
2012-04-01 18:12:16 +0200

    unit 1 montecarlo localization

--- src/Unit1.py
+++ src/Unit1.py
@@ -1,13 +1,23 @@
 #initial probabilities
-p = [0.2, 0.2, 0.2, 0.2, 0.2]
+from cherrypy.test.test_conn import pov
 #World`s map
 world = ['green', 'red', 'red', 'green', 'green']
+#initial distributions
+p = [1./len(world)]*len(world)
 #Measurement
-measurement = ['red','green']
+measurement = ['red','red']
+motions = [1,1]
 #Probability of sensnig state and being in that state
 pHit = 0.6
 #Probablity of sensing state and being in other state
 pMiss = 0.2
+#Add exact probability
+pExact = 0.8
+#Add overshoot probability
+pOvershoot = 0.1
+#Add undershoot probability
+pUndershoot = 0.1
+
 def sense(p, Z):#Define a function sense return q
     q = []
     for i in range(len(world)):
@@ -19,11 +29,14 @@ def sense(p, Z):#Define a function sense return q
 def move(p,U):
     q = []
     for i in range(len(p)):
-        q.append(p[(i-U) % len(p)])
+        s =  pExact * p[(i-U) % len(p)]
+        s += pOvershoot * p[(i-U-1) % len(p)]
+        s += pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
     return q
 
-for Z in measurement:   
-    p = sense(p, Z)
+for i in range(len(measurement)):
+    p = sense(p, measurement[i])
+    p = move(p, motions[i])
     
-p = move(p,-1)    
 print p
